cc19f0bde4beff013febd5b0a6ad357ee341607f
MisratIra <misratmehenaj199@gmail.com>
2022-10-16 05:38:19 +0600

    hello in button

--- src/components/ShopItems/ShopItems.js
+++ src/components/ShopItems/ShopItems.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const ShopItems = ({ item }) => {
-    const { name, picture, price } = item;
-    return (
-        <div className='relative'>
-            <img className='w-full h-96 rounded hover:scale-x-125 duration-700 ease-in-out' src={picture} alt="" />
-            <p className='text-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
-                <button className='bg-lime-700 px-4 rounded'> {name}</button>
-            </p>
-        </div>
-    );
+	const { name, picture, price } = item;
+	return (
+		<div className="relative">
+			<img
+				className="w-full h-96 rounded hover:scale-x-125 duration-700 ease-in-out"
+				src={picture}
+				alt=""
+			/>
+			<p className="text-2xl absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+				<button className="bg-lime-700 px-4 rounded"> Hello {name}</button>
+			</p>
+		</div>
+	);
 };
 
 export default ShopItems;
