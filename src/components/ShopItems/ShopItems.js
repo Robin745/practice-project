31e870d64db172ce564811fc511f41f5a8deed3d
Robin745 <moshiur251@gmail.com>
2022-10-16 05:17:59 +0600

    updated

--- src/components/ShopItems/ShopItems.js
+++ src/components/ShopItems/ShopItems.js
@@ -4,7 +4,7 @@ const ShopItems = ({ item }) => {
     const { name, picture, price } = item;
     return (
         <div className='relative'>
-            <img className='w-80 h-4/5 rounded' src={picture} alt="" />
+            <img className='w-80  h-4/5 rounded' src={picture} alt="" />
             <button className='text-2xl absolute inset-0'> {name}</button>
 
         </div>
