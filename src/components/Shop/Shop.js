dbbc90937ceaf4a20bd1ffcf258ffbbe7096df5a
MisratIra <misratmehenaj199@gmail.com>
2022-10-16 02:46:24 +0600

    latest-commit

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -8,6 +8,7 @@ const Shop = () => {
 	return (
 		<div className="mt-4">
 			<h1 className="text-4xl">Hey, hello welcome to new pull request</h1>
+			<h3>This the latest commit we made</h3>
 			<h2 className="text-2xl">Welcome to shop {products.length}</h2>
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 mt-8 md:mx-12">
 				{products.map((item) => (
